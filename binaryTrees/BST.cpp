0adf72ab4ef237df1def2d316f617157ebec1363
nirvanafan205 <nirvanafan205@gmail.com>
2023-06-21 10:00:16 -0700

    added main function and deallocation function

--- binaryTrees/BST.cpp
+++ binaryTrees/BST.cpp
@@ -5,11 +5,11 @@ using namespace std;
 struct Node {
 	int data;
 	struct Node *left, *right;
-}
+};
 
 Node* newNode(int data)
 {
-	Node* temp = newNode;
+	Node* temp = new Node();
 
 	temp->data = data;
 
@@ -37,13 +37,13 @@ void printInorder(struct Node* node)
 }
 
 //preOrder traversal
-void print PreOrder(struct Node* node)
+void printPreOrder(struct Node* node)
 {
 	if(node == NULL)
 	{
 		return;
 	}
-	
+
 	//print data
 	cout << node->data << " ";
 
@@ -66,4 +66,47 @@ void printPostOrder(struct Node* node)
 	printPostOrder(node->right);
 
 	cout << node->data << " ";
+}
+
+void deallocateTree(Node* root)
+{
+	if(root == nullptr)
+	{
+		return;
+	}
+
+	deallocateTree(root->left);
+	deallocateTree(root->right);
+
+	delete root;
+}
+
+
+int main()
+{
+	// create binary tree
+	Node* root = newNode(1);
+	root->left = newNode(2);
+	root->right = newNode(3);
+	root->left->left = newNode(4);
+	root->left->right = newNode(5);
+
+	// perform inorder traversal
+	cout << "Inorder Traversal: ";
+	printInorder(root);
+	cout << endl; 
+
+	// perform postorder traversal
+	cout << "Postorder Traversal: ";
+	printPostOrder(root);
+	cout << endl;
+
+
+	// perform postorder traversal
+	cout << "Preorder Traversal: ";
+	printPreOrder(root);
+	cout << endl;
+
+	// deallocate memory
+	deallocateTree(root);
 }
